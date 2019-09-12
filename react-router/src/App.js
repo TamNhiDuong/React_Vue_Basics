66d6edc73189ffa70f35783f7bf11604fb3d7741
TamNhiDuong <yanxinyee@gmail.com>
2019-09-12 19:44:41 +0300

    react router

---
+++ react-router/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as BrowserRounter, Route, Switch } from "react-router-dom"; 
+import Navigator from './Navigator';
+import"bootstrap/dist/css/bootstrap.min.css";
+
+function Index() {
+  return <h2>This is Home Page</h2>;
+}
+function About() {
+  return <h2>This is About Page</h2>;
+}
+function Contact() {
+  return <h2>This is Contact Address Page</h2>; 
+}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Welcome to React Router</header>
+  <BrowserRounter>
+    <div>
+      <Navigator />
+      <Switch>
+      <Route path="/" exact component={Index} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route render={() => <h1>Page not found</h1>} />
+      </Switch>
+      
+    </div>
+  </BrowserRounter>
+  </div>
+  );
+}
+
+export default App;
---
+++ react-router/src/Navigator.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from "react-router-dom"; 
+import"bootstrap/dist/css/bootstrap.min.css";
+const Navigator = props => {
+    return (
+        
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            
+            <Link className="navbar-brand" to ="/" >My React Page</Link>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id ="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/about/">About</Link>
+              </li>
+
+              <li className="nav -item">
+                <Link className="nav-link" to="/contact/">Contact</Link>
+              </li>
+        </ul>
+        </div>
+        </nav>
+        
+    );
+};
+export default Navigator; 
+
