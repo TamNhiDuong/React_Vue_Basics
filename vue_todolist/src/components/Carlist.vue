ca62264497c2d2938c914562693f49bb0b6582e4
TamNhiDuong <yanxinyee@gmail.com>
2019-10-10 10:30:06 +0300

    Array of objects

---
+++ vue_todolist/src/components/Carlist.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <table>
+        <tbody>
+            <tr v-for="(car, index) in cars" v-bind:key="index">
+                
+        </tbody>
+    </table>
+</div>
+    
+</template>
+
+<script>
+import axios from 'axios'; 
+
+export default {
+    name: 'Carlist',
+    data(){
+        return {
+            cars: []
+        }
+    },
+    mounted() {
+        axios.get('https://carstockrest.herokuapp.com/cars')
+        .then(response => (this.cars = response.data._embedded.cars))
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue_todolist/src/components/TodoList.vue
+++ vue_todolist/src/components/TodoList.vue
@@ -2,12 +2,13 @@
 <!--v-model save directly to data called "description".-->
 <div>
 <input type="text" placeholder="New todo" v-model="description" />
+<input type="date" placeholder="Date" v-model="date" />
 <button v-on:click="add">Add</button><br/>
 
 <table>
     <tbody>
-        <tr v-for="(todo, index) in todos" 
-        v-bind:key="index"><td>{{todo}}</td></tr>
+        <tr v-for="(todo, index) in todos" v-bind:key="index">
+        <td>{{todo.description}} {{todo.date}}</td></tr>
     </tbody>
 </table>
 </div>
@@ -20,13 +21,15 @@ export default {
     data() {
         return {
         todos: [],
-        description: ''
+        description: '',
+        date: ''
         }
     },
     methods: {
         add: function() {
-            this.todos.push(this.description);
-            this.description = ''
+            this.todos.push({description: this.description, date: this.date});
+            this.description= '';
+            this.date= ''
         }
     } 
 }
